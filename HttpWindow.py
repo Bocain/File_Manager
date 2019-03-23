5ac02cde8d842e0cc1c5408536415d48dfa086f4
Kamil <allegrodolar@op.pl>
2019-03-23 18:04:52 +0100

    refactor zakonczony na HttpWindow

--- HttpWindow.py
+++ HttpWindow.py
@@ -9,44 +9,43 @@ import json
 class HttpWindow(gtk.Window):
     def __init__(self):
         super(HttpWindow, self).__init__()
-
-        self.catalogComboBox = ''
+        self.chosenCatalogComboBox = ''
         
         self.set_title("Dodaj do katalogu nowa zakladke z linkiem www.")
         self.set_default_size(350, 200)
         self.set_position(gtk.WIN_POS_CENTER)
 
-        lb_katalog = gtk.Label('Wybierz z rozwijanej listy katalog, w ktorym dodasz nowa zakladke.')
+        labelCatalog = gtk.Label('Wybierz z rozwijanej listy katalog, w ktorym dodasz nowa zakladke.')       
+        catalogList = gtk.combo_box_new_text()
+        catalogList.connect('changed', self.comboOption)
         
-        cb = gtk.combo_box_new_text()
-        cb.connect('changed', self.comboOption)
         with open('json_test.json') as json_file:
-            caly_slownik = json.load(json_file)
-        klucze = caly_slownik.keys()
-        for tab in klucze:
-            cb.append_text(str(tab))
+            data = json.load(json_file)
+        catalogsAsKeys = data.keys()
+        for catalogAsKey in catalogsAsKeys:
+            catalogList.append_text(str(catalogAsKey))
         
-        lb_1 = gtk.Label('Wpisz nazwe nowej zakladki :')
-        self.entry_1 = gtk.Entry()
+        labelNewTab = gtk.Label('Wpisz nazwe nowej zakladki :')
+        self.entryNewTab = gtk.Entry()
         
-        lb_2 = gtk.Label('Wklej lub wpisz adres strony internetowej :')
-        self.entry_2 = gtk.Entry()
+        labelNewLink = gtk.Label('Wklej lub wpisz adres strony internetowej :')
+        self.entryNewLink = gtk.Entry()
         
-        btn_add = gtk.Button('Dodaj')
-        btn_add.connect('clicked', self.button_add)
+        buttonConfirmAdd = gtk.Button('Dodaj')
+        buttonConfirmAdd.connect('clicked', self.AddingConfirmation)
         
         screen = gtk.Fixed()
 
-        screen.put(lb_katalog, 10, 10)
-        screen.put(cb, 10, 30)
+        screen.put(labelCatalog, 10, 10)
+        screen.put(catalogList, 10, 30)
                
-        screen.put(lb_1, 10, 60)
-        screen.put(self.entry_1, 10, 80)
+        screen.put(labelNewTab, 10, 60)
+        screen.put(self.entryNewTab, 10, 80)
 
-        screen.put(lb_2, 10, 120)
-        screen.put(self.entry_2, 10, 140)
+        screen.put(labelNewLink, 10, 120)
+        screen.put(self.NewLink, 10, 140)
         
-        screen.put(btn_add, 10, 170)
+        screen.put(buttonConfirmAdd, 10, 170)
 
         self.add(screen)
         self.show_all()
@@ -54,19 +53,15 @@ class HttpWindow(gtk.Window):
         return
 
     def comboOption(self, widget):
-        self.catalogComboBox = widget.get_active_text()
+        self.chosenCatalogComboBox = widget.get_active_text()
 
-    def button_add(self, widgets):
+    def AddingConfirmation(self, widgets):
         with open('json_test.json') as json_file:
-            caly_slownik = json.load(json_file)
-            
-        caly_slownik[str(self.catalogComboBox)][str(self.entry_1.get_text())] = str(self.entry_2.get_text()) 
-
+            data = json.load(json_file)           
+        data[str(self.chosenCatalogComboBox)][str(self.entryNewTab.get_text())] = str(self.NewLink.get_text())
         with open('json_test.json', 'w') as outfile:
-            json.dump(caly_slownik, outfile)
-            
-        print "Doda³eœ now¹ zak³adkê"
+            json.dump(data, outfile)
 
-def odpal():
+def launch():
     HttpWindow()
     gtk.main()
