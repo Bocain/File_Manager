443228fa4f123afc8836185800c41a93e74db6b5
Kamil <allegrodolar@op.pl>
2019-03-24 08:53:38 +0100

    refaktor zakończony na LinkFileWindow

--- LinkFileWindow.py
+++ LinkFileWindow.py
@@ -10,78 +10,60 @@ import FilesWindow
 class LinkFileWindow(gtk.Window):
 
     chosenFilePath = 'Miejsce na sciezke pliku'
-    wybrany_katalog = ''
+    chosenCatalog = ''
     
     def __init__(self):
         super(LinkFileWindow, self).__init__()
-
-        self.catalogComboBox = ''
-        
+        self.catalogComboBox = ''   
         self.set_title("Dodaj do katalogu nowa zakladke z plikiem.")
         self.set_default_size(350, 200)
         self.set_position(gtk.WIN_POS_CENTER)
-
-        lb_katalog = gtk.Label('Wybierz z rozwijanej listy katalog, w ktorym dodasz nowa zakladke.')
-
-        
-        cb = gtk.combo_box_new_text()
-        cb.connect('changed', self.comboOption)
+        labelCatalog = gtk.Label('Wybierz z rozwijanej listy katalog, w ktorym dodasz nowa zakladke.')
+        catalogsList = gtk.combo_box_new_text()
+        catalogsList.connect('changed', self.chooseCatalog)
 
         with open('Database.json') as json_file:
-            caly_slownik = json.load(json_file)
-        klucze = caly_slownik.keys()
-        for tab in klucze:
-            cb.append_text(str(tab))
+            data = json.load(json_file)
+        catalogsAsKeys = data.keys()
+        for cak in catalogsAsKeys:
+            catalogsList.append_text(str(cak))
         
-        lb_1 = gtk.Label('Wpisz nazwe nowej zakladki :')
-        self.entry_1 = gtk.Entry()
-        
-        lb_2 = gtk.Label('Wklej lub wpisz sciezke pliku :')
-        self.entry_2 = gtk.Entry()
-        self.entry_2.set_text(LinkFileWindow.chosenFilePath)
+        newTab = gtk.Label('Wpisz nazwe nowej zakladki :')
+        self.newTabName = gtk.Entry()    
+        newPath = gtk.Label('Wklej lub wpisz sciezke pliku :')
+        self.newTabPath = gtk.Entry()
+        self.newTabPath.set_text(LinkFileWindow.chosenFilePath)
+        buttonFindFile = gtk.Button('Znajdz plik')
+        buttonFindFile.connect('clicked', self.windowFilesDriveC)
+        buttonAddTab = gtk.Button('Dodaj')
+        buttonAddTab.connect('clicked', self.addingTabWithPath)
 
-        btn_file = gtk.Button('Znajdz plik')
-        btn_file.connect('clicked', self.button_find_file)
-        
-        btn_add = gtk.Button('Dodaj')
-        btn_add.connect('clicked', self.button_add)
-        
         screen = gtk.Fixed()
-
-        screen.put(lb_katalog, 10, 10)
-        screen.put(cb, 10, 30)
-               
-        screen.put(lb_1, 10, 60)
-        screen.put(self.entry_1, 10, 80)
-
-        screen.put(lb_2, 10, 120)
-        screen.put(self.entry_2, 10, 140)
-        
-        screen.put(btn_file, 220, 140)
-        screen.put(btn_add, 10, 170)
-
+        screen.put(labelCatalog, 10, 10)
+        screen.put(catalogsList, 10, 30)
+        screen.put(newTab, 10, 60)
+        screen.put(self.newTabName, 10, 80)
+        screen.put(newPath, 10, 120)
+        screen.put(self.newTabPath, 10, 140)
+        screen.put(buttonFindFile, 220, 140)
+        screen.put(buttonAddTab, 10, 170)
         self.add(screen)
         self.show_all()
         return
 
-    def comboOption(self, widget):
+    def chooseCatalog(self, widget):
         self.catalogComboBox = widget.get_active_text()
-        LinkFileWindow.wybrany_katalog = widget.get_active_text()
+        LinkFileWindow.chosenCatalog = widget.get_active_text()
         
-
-    def button_add(self, widgets):
+    def addingTabWithPath(self, widgets):
         with open('Database.json') as json_file:
-            caly_slownik = json.load(json_file)
-            
-        caly_slownik[str(self.catalogComboBox)][str(self.entry_1.get_text())] = str(self.entry_2.get_text()) 
-
+            data = json.load(json_file)
+        data[str(self.catalogComboBox)][str(self.newTabName.get_text())] = str(self.newTabPath.get_text()) 
         with open('Database.json', 'w') as outfile:
-            json.dump(caly_slownik, outfile)
+            json.dump(data, outfile)
 
-    def button_find_file(self, widgets):
+    def windowFilesDriveC(self, widgets):
         FilesWindow.launch()
-        print('Otwiera okno z plikami')
-
 
 def launch():
     LinkFileWindow()
